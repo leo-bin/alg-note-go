f50cbc1eed57a45ded579311c96c806ed35a1119
abin <abinbin.0413@gmail.com>
2024-07-29 23:21:33 +0800

    21、86

---
+++ leetcode/list_partition_86_test.go
@@ -0,0 +1,118 @@
+package leetcode
+
+import "testing"
+
+// 分隔链表 leetcode 86
+// https://leetcode.cn/problems/partition-list/
+// 题目介绍：
+// 给你一个链表的头节点head和一个特定值x，请你对链表进行分隔，使得所有小于x的节点都出现在大于或等于x的节点之前
+// 你应当保留两个分区中每个节点的初始相对位置
+//
+// 示例1：
+// 输入：head = [1,4,3,2,5,2], x = 3
+// 输出：[1,2,2,4,3,5]
+//
+// 示例 2：
+// 输入：head = [2,1], x = 2
+// 输出：[1,2]
+func Test_ListPartition(t *testing.T) {
+	testCaseList := []struct {
+		Head         *ListNode
+		X            int
+		ExpectResult *ListNode
+	}{
+		0: {
+			Head: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val: 2,
+							Next: &ListNode{
+								Val: 5,
+								Next: &ListNode{
+									Val:  2,
+									Next: nil,
+								},
+							},
+						},
+					},
+				},
+			},
+			X: 3,
+			ExpectResult: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 4,
+							Next: &ListNode{
+								Val: 3,
+								Next: &ListNode{
+									Val:  5,
+									Next: nil,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for i, testCase := range testCaseList {
+		head := testCase.Head
+		x := testCase.X
+		expectResult := testCase.ExpectResult
+		actualResult := listPartition(head, x)
+		if actualResult == nil {
+			t.Errorf("excute err")
+		}
+		e := expectResult
+		a := actualResult
+		for e != nil && a != nil {
+			if e.Val != a.Val {
+				t.Errorf("test case %v,not passed", i)
+			}
+			e = e.Next
+			a = a.Next
+		}
+		t.Logf("test case %v,passed", i)
+	}
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func listPartition(head *ListNode, x int) *ListNode {
+	leftDummy := &ListNode{}
+	rightDummy := &ListNode{}
+	left := leftDummy
+	right := rightDummy
+
+	for head != nil {
+		// 找比x要小的
+		if head.Val < x {
+			left.Next = head
+			left = left.Next
+		} else {
+			// 找大于或等于x的
+			right.Next = head
+			right = right.Next
+		}
+		// head指针往后移
+		head = head.Next
+	}
+	// 清除右链表的最后一个节点指向，防止成环
+	right.Next = nil
+	// 连接左右链表
+	left.Next = rightDummy.Next
+
+	return leftDummy.Next
+}
---
+++ leetcode/merge_two_list_21_test.go
@@ -0,0 +1,118 @@
+package leetcode
+
+import "testing"
+
+// 合并两个有序链表，leetcode 21
+// https://leetcode.cn/problems/merge-two-sorted-lists/
+// 题目介绍：
+// 将两个升序链表合并为一个新的 升序 链表并返回
+// 新链表是通过拼接给定的两个链表的所有节点组成的
+//
+// 示例 1：
+// 输入：l1 = [1,2,4], l2 = [1,3,4]
+// 输出：[1,1,2,3,4,4]
+//
+// 示例 2：
+// 输入：l1 = [], l2 = []
+// 输出：[]
+//
+// 示例 3：
+// 输入：l1 = [], l2 = [0]
+// 输出：[0]
+
+func Test_MergeTwoLists(t *testing.T) {
+	testCaseList := []struct {
+		List1        *ListNode
+		List2        *ListNode
+		ExpectResult *ListNode
+	}{
+		0: {
+			List1: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 4,
+					},
+				},
+			},
+			List2: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 3,
+					Next: &ListNode{
+						Val: 4,
+					},
+				},
+			},
+			ExpectResult: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val: 4,
+								Next: &ListNode{
+									Val:  4,
+									Next: nil,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for i, testCase := range testCaseList {
+		list1 := testCase.List1
+		list2 := testCase.List2
+		expectResult := testCase.ExpectResult
+		actualResult := mergeTwoLists(list1, list2)
+		if actualResult == nil {
+			t.Errorf("excute err")
+		}
+		e := expectResult
+		a := actualResult
+		for e != nil && a != nil {
+			if e.Val != a.Val {
+				t.Errorf("test case %v,not passed", i)
+			}
+			e = e.Next
+			a = a.Next
+		}
+		t.Logf("test case %v,passed", i)
+	}
+}
+
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	cur := dummy
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			cur.Next = list1
+			list1 = list1.Next
+		} else {
+			cur.Next = list2
+			list2 = list2.Next
+		}
+		// 移动cur指针
+		cur = cur.Next
+	}
+	// 最后剩下的节点直接放到最后拼接起来
+	if list1 != nil {
+		cur.Next = list1
+	} else {
+		cur.Next = list2
+	}
+
+	return dummy.Next
+}
