33746cf0b2a0115709ff688d84f4b724e12dc573
abin <abinbin.0413@gmail.com>
2024-08-06 22:23:21 +0800

    19，删除链表倒数第N个节点,v2

--- leetcode/remove_nth_node_from_end_19_test.go
+++ leetcode/remove_nth_node_from_end_19_test.go
@@ -46,3 +46,26 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 
 	return dummy.Next
 }
+
+// removeNthFromEndV2 双指针思想（快慢指针）
+// 指针p1和p2，p1先走n步（剩余路径就是k-n），然后p2和p1同时开始走，当p1走完时，p2就走了k-n步
+// 此时p2所在的位置就是目标节点的位置，直接删除即可
+// 注意，我们找到的是目标节点，但删除要知道目标节点的前置节点，所以要让p2少走一步
+// 这里通过设置虚拟头结点来实现
+func removeNthFromEndV2(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0, head}
+	p1, p2 := head, dummy
+	// 1.先让p1先走n步
+	for i := 0; i < n; i++ {
+		p1 = p1.Next
+	}
+	// 2.p2开始跟着p1走，直到p1结束
+	for p1 != nil {
+		p1 = p1.Next
+		p2 = p2.Next
+	}
+	// 3.此时的p2就是目标节点的前置节点，直接删除即可
+	p2.Next = p2.Next.Next
+
+	return dummy.Next
+}
