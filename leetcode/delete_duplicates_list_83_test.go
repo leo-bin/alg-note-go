475382ec48704e6a1a5f7d273cd04f3a8c50edc6
abin <abinbin.0413@gmail.com>
2024-09-02 23:32:12 +0800

    26、83，数组，快慢指针

---
+++ leetcode/delete_duplicates_list_83_test.go
@@ -0,0 +1,80 @@
+package leetcode
+
+import "testing"
+
+// 删除排序链表中的重复元素 leetcode-83
+// 链接：https://leetcode.cn/problems/remove-duplicates-from-sorted-list/
+// 题目描述：
+// 给定一个已排序的链表的头 head
+// 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表
+//
+// 示例1：
+// 输入：head = [1,1,2]
+// 输出：[1,2]
+//
+// 示例2：
+// 输入：head = [1,1,2,3,3]
+// 输出：[1,2,3]
+
+func Test_DeleteDuplicatesList(t *testing.T) {
+
+}
+
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
+
+// deleteDuplicates hashmap去重 时间O（n），空间O（n）
+func deleteDuplicates(head *ListNode) *ListNode {
+	// base case
+	if head == nil {
+		return nil
+	}
+	if head.Next == nil {
+		return head
+	}
+
+	nodeSet := make(map[int]struct{})
+	p := head
+	newHead := &ListNode{}
+	tmp := newHead
+	for p != nil {
+		if _, exits := nodeSet[p.Val]; !exits {
+			nodeSet[p.Val] = struct{}{}
+			tmp.Next = &ListNode{Val: p.Val, Next: nil}
+			tmp = tmp.Next
+		}
+		p = p.Next
+	}
+
+	return newHead.Next
+}
+
+// deleteDuplicates 快慢指针原地修改链表解法，时间O（n），空间O（1）
+// 思想：
+// 1.慢指针修改链表的关系，快指针在前面探路，查找重复的元素
+// 2.slow和fast同时出发，当遇到连续重复的元素时，fast继续往前走，slow不动
+// 3.当出现和slow不同的元素时（此时找到了下一个不重复的元素），需要修改链表的关系，将slow指向fast，并断开slow原来的关系
+func deleteDuplicatesV2(head *ListNode) *ListNode {
+	// base case
+	if head == nil {
+		return nil
+	}
+	if head.Next == nil {
+		return head
+	}
+
+	slow, fast := head, head
+	for fast != nil {
+		if fast.Val != slow.Val {
+			slow.Next = fast
+			slow = fast
+		}
+		fast = fast.Next
+	}
+	// 最后一个节点如果也是连续的，需要去重
+	slow.Next = nil
+
+	return head
+}
---
+++ leetcode/remove_duplicates_26_test.go
@@ -0,0 +1,53 @@
+package leetcode
+
+import "testing"
+
+// 删除有序数组中的重复项 leetcode-26
+// https://leetcode.cn/problems/remove-duplicates-from-sorted-array/description/
+// 题目介绍：
+// 给你一个非严格递增排列的数组nums
+// 请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+// 元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数
+// 考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过
+// 更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要
+// 返回 k
+//
+// 示例1：
+// 输入：nums = [1,1,2]
+// 输出：2, nums = [1,2,_]
+// 解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素
+//
+// 示例 2：
+// 输入：nums = [0,0,1,1,1,2,2,3,3,4]
+// 输出：5, nums = [0,1,2,3,4]
+// 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素
+
+func Test_RemoveDuplicates(t *testing.T) {
+
+}
+
+// removeDuplicates 快慢指针
+// 思想：
+// 1.数组有序，说明重复的元素一定是连续出现
+// 2.要求原地修改，不能使用额外容器进行去重判断
+// 3.快慢指针思想，快指针在前面探路，慢指针在后面进行修改和标记
+// 两个指针从第一个元素开始一起前进，当快指针所在的元素和slow指针不一样，说明找到了下一段重复元素
+// 此时slow指针往前走一步，同时用fast指针的元素和slow进行替换
+func removeDuplicates(nums []int) int {
+	// base case
+	if len(nums) <= 0 {
+		return 0
+	}
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		// 下一个不重复的元素出现
+		if nums[fast] != nums[slow] {
+			slow++
+			// 原地修改数组
+			nums[slow] = nums[fast]
+		}
+		fast++
+	}
+	// 此时的0-slow就是修改后的数组
+	return slow + 1
+}
