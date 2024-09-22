7c3bed2c0ad43732f1e21a819aac0a7e15486114
abin <abinbin.0413@gmail.com>
2024-09-22 16:56:06 +0800

    dfs

--- leetcode/二叉树类问题/flatten_tree2list_114_test.go
+++ leetcode/二叉树类问题/flatten_tree2list_114_test.go
@@ -27,5 +27,20 @@ func preBuild(root, p *TreeNode) {
 
 // flattenV2
 func flattenV2(root *TreeNode) {
+	var prePath []*TreeNode
+	preBuildV2(root, prePath)
+	for i := 1; i < len(prePath); i++ {
+		pre, cur := prePath[i-1], prePath[i]
+		// 构建链表关系
+		pre.Left, pre.Right = nil, cur
+	}
+}
 
+func preBuildV2(root *TreeNode, prePath []*TreeNode) {
+	if root == nil {
+		return
+	}
+	prePath = append(prePath, root)
+	preBuildV2(root.Left, prePath)
+	preBuildV2(root.Right, prePath)
 }
