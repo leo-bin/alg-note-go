044c928c18993405cbf76b0d84eaa54c96621db8
abin <abinbin.0413@gmail.com>
2024-10-08 23:19:23 +0800

    dp,买卖股票的最佳时机

---
+++ leetcode/动态规划问题/dp_max_profit_iii_123_test.go
@@ -0,0 +1,69 @@
+package 动态规划问题
+
+import (
+	"testing"
+)
+
+// 买卖股票的最佳时机iii，123，hard
+// https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iii/
+// 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格
+// 设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易
+// 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+//
+// 示例 1:
+// 输入：prices = [3,3,5,0,0,3,1,4]
+// 输出：6
+// 解释：在第4天（股票价格=0）的时候买入，在第6天（股票价格=3）的时候卖出，这笔交易所能获得利润 = 3-0 = 3
+// 随后，在第7天（股票价格=1）的时候买入，在第8天（股票价格=4）的时候卖出，这笔交易所能获得利润 = 4-1 = 3
+//
+// 示例 2：
+// 输入：prices = [1,2,3,4,5]
+// 输出：4
+// 解释：在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4
+// 注意你不能在第1天和第2天接连购买股票，之后再将它们卖出
+// 因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票
+//
+// 示例 3：
+// 输入：prices = [7,6,4,3,1]
+// 输出：0
+// 解释：在这个情况下, 没有交易完成, 所以最大利润为 0。
+//
+// 示例 4：
+// 输入：prices = [1]
+// 输出：0
+
+func Test_MaxProfitIII(t *testing.T) {
+}
+
+func maxProfitIII(prices []int) int {
+	maxK := 2
+	dp := make([][][]int, len(prices))
+	for i, _ := range prices {
+		dp[i] = make([][]int, maxK+1)
+		for k := 0; k < maxK+1; k++ {
+			dp[i][k] = make([]int, 2)
+		}
+	}
+	for i, price := range prices {
+		for k := maxK; k >= 1; k-- {
+			// 特殊处理第一天
+			if i-1 == -1 {
+				dp[0][k][0] = 0
+				dp[0][k][1] = -price
+				continue
+			}
+			// 第i天，k次交易次数，卖出股票
+			dp[i][k][0] = maxIntP3(dp[i-1][k][0], dp[i-1][k][1]+price)
+			// 第i天，k次交易次数，买入股票
+			dp[i][k][1] = maxIntP3(dp[i-1][k][1], dp[i-1][k-1][0]-price)
+		}
+	}
+	return dp[len(prices)-1][2][0]
+}
+
+func maxIntP3(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
